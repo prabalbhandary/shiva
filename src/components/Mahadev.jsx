eebb2e7588b3fa62505f56ec1944986befc164df
prabalbhandary <prabalbhandary97@gmail.com>
2024-08-08 15:05:50 +0545

    Har Har Mahadev

--- src/components/Mahadev.jsx
+++ src/components/Mahadev.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import './Mahadev.css'
+import React from 'react';
+import './Mahadev.css';
 
 const Mahadev = () => {
   return (
     <>
-        <div className="main">
-            <p className="mahamantra">ॐ नमः शिवाय</p>
-            <div className="shiv-ling">
-                <div className="one"></div>
-                <div className="two"></div>
-                <div className="red-dot"></div>
-                <div className="three"></div>
-            </div>
-            <div className="oval-stand">
-                <div className="block-fill"></div>
-            </div>
+      <div className="main">
+        <p className="mahamantra">ॐ नमः शिवाय</p>
+        <div className="shiv-ling">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="red-dot"></div>
+          <div className="three"></div>
         </div>
-        <p className='basetext'>🕉️🔱❤️हर हर महादेव❤️🔱🕉️</p>
+        <div className="oval-stand">
+          <div className="block-fill"></div>
+        </div>
+      </div>
+      <p className='basetext'>🕉️🔱❤️हर हर महादेव❤️🔱🕉️</p>
     </>
-  )
-}
+  );
+};
 
-export default Mahadev
+export default Mahadev;
